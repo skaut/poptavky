70c33ba1e16cea7773957beb4d2f64fe0d4d7b8b
Marek Dědič <developer@dedic.eu>
2021-07-23 14:40:43 +0200

    Added fs mock

--- packages/collector/__tests__/getGlobalConfig.test.ts
+++ packages/collector/__tests__/getGlobalConfig.test.ts
@@ -1,6 +1,12 @@
+import * as fs from "fs";
+import { mocked } from "ts-jest/utils";
+
 import { getGlobalConfig } from "../src/getGlobalConfig";
 
+jest.mock("fs");
+
 test("getGlobalConfig loads a file", () => {
+  mocked(fs).readFileSync.mockReturnValue('{"projects": []}');
   expect(getGlobalConfig()).toStrictEqual({
     projects: [{ owner: "skaut", repo: "poptavky-test" }],
   });
