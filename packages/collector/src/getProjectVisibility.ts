0acd78a540a607f9c7cfb1f9e2ae8c646bd00d20
Marek Dědič <developer@dedic.eu>
2021-07-25 21:47:45 +0200

    Added VisibilityError

--- packages/collector/src/getProjectVisibility.ts
+++ packages/collector/src/getProjectVisibility.ts
@@ -2,7 +2,13 @@ import { octokit } from "./octokit";
 
 import { Project } from "./interfaces/Project";
 
+import { VisibilityError } from "../src/exceptions/VisibilityError";
+
 export async function getProjectVisibility(project: Project): Promise<boolean> {
-  const rawResponse = await octokit.rest.repos.get({ ...project });
+  const rawResponse = await octokit.rest.repos
+    .get({ ...project })
+    .catch(function (e): never {
+      throw new VisibilityError(String(e));
+    });
   return !rawResponse.data.private;
 }
