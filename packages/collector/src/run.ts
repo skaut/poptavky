3fd5034f97d2a3abf3e79644455b8e74a7f0468c
Marek Dědič <developer@dedic.eu>
2021-07-25 23:10:29 +0200

    split run() into own module

---
+++ packages/collector/src/run.ts
@@ -0,0 +1,34 @@
+import * as fs from "fs";
+import * as core from "@actions/core";
+
+import { getGlobalConfig } from "./getGlobalConfig";
+import { getProjectListing } from "./getProjectListing";
+
+import { ProjectListings } from "./interfaces/ProjectListings";
+
+import { PoptavkyError } from "./exceptions/PoptavkyError";
+
+export async function run() {
+  try {
+    const globalConfig = getGlobalConfig();
+    const listings: ProjectListings = { projects: [] };
+    for (const project of globalConfig.projects) {
+      try {
+        listings.projects.push(await getProjectListing(project));
+      } catch (e) {
+        core.error(
+          "There was an error while processing the project " +
+            project.owner +
+            "/" +
+            project.repo +
+            ". " +
+            (e as PoptavkyError).message
+        );
+      }
+    }
+    fs.writeFileSync("listings.json", JSON.stringify(listings));
+  } catch (e) {
+    process.exitCode = 1;
+    core.setFailed((e as PoptavkyError).message);
+  }
+}
