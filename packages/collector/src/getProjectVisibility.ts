5625b06e5a69f83ef959dca3f31ec9aad353116d
Marek Dědič <developer@dedic.eu>
2021-07-28 22:08:01 +0200

    Fixed an arrow function

--- packages/collector/src/getProjectVisibility.ts
+++ packages/collector/src/getProjectVisibility.ts
@@ -7,7 +7,7 @@ import { VisibilityError } from "../src/exceptions/VisibilityError";
 export async function getProjectVisibility(project: Project): Promise<boolean> {
   const rawResponse = await octokit.rest.repos
     .get({ ...project })
-    .catch(function (e): never {
+    .catch((e): never => {
       throw new VisibilityError(String(e));
     });
   return !rawResponse.data.private;
