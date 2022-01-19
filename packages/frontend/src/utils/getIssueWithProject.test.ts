050ec900a2621856610f41591742b443f5897357
Dominik Bláha <dominik.blaha@avast.com>
2022-01-19 15:37:01 +0100

    Test utils

---
+++ packages/frontend/src/utils/getIssueWithProject.test.ts
@@ -0,0 +1,39 @@
+import { testData } from "../testData"
+import { getIssueWithProject } from "./getIssueWithProject"
+
+const project = testData.projects[0]
+
+it("should get issue with project", () => {
+  const result = getIssueWithProject(
+    testData,
+    project.owner,
+    project.repo,
+    project.issues[0].number
+  )
+  expect(result).toMatchSnapshot()
+})
+
+it("should not get issue with project if there is no existing owner", () => {
+  const result = getIssueWithProject(
+    testData,
+    "",
+    project.repo,
+    project.issues[0].number
+  )
+  expect(result).toBe(undefined)
+})
+
+it("should not get issue with project if there is no existing repo", () => {
+  const result = getIssueWithProject(
+    testData,
+    project.owner,
+    "",
+    project.issues[0].number
+  )
+  expect(result).toBe(undefined)
+})
+
+it("should not get issue with project if there is no existing issue number", () => {
+  const result = getIssueWithProject(testData, project.owner, project.repo, 99)
+  expect(result).toBe(undefined)
+})
