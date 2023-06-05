47850c7f39316c6e2a46c2132717b5aae0faec48
Marek Dědič <developer@dedic.eu>
2023-06-05 11:38:01 +0200

    Using default node_modules location

--- packages/frontend/config/webpack.config.js
+++ packages/frontend/config/webpack.config.js
@@ -224,11 +224,6 @@ module.exports = function (webpackEnv) {
       ],
     },
     resolve: {
-      // This allows you to set a fallback for where webpack should look for modules.
-      // We placed these paths second because we want `node_modules` to "win"
-      // if there are any conflicts. This matches Node resolution mechanism.
-      // https://github.com/facebook/create-react-app/issues/253
-      modules: ["node_modules", paths.appNodeModules].concat("" || []),
       // These are the reasonable defaults supported by the Node ecosystem.
       // We also include JSX as a common component filename extension to support
       // some tools, although we do not recommend using it, see:
