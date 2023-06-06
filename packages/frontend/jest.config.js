8451477d3485e77a13a16dea32b0baa27a4b2322
Marek Dědič <developer@dedic.eu>
2023-06-06 11:06:18 +0200

    Merged jest config into one file

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
   collectCoverage: true,
   collectCoverageFrom: [
     "src/**/*.{ts,tsx}",
@@ -7,7 +6,7 @@ module.exports = {
     "!src/config.ts",
     "!src/globalStyles.ts",
   ],
-  setupFiles: ["react-app-polyfill/jsdom"],
+  //preset: "ts-jest/presets/default-esm",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testMatch: [
     "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
@@ -15,32 +14,25 @@ module.exports = {
   ],
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$": "<rootDir>/config/jest/babelTransform.js",
-    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-    "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)":
-      "<rootDir>/config/jest/fileTransform.js",
+    "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$": [
+      "babel-jest",
+      {
+        presets: [
+          [
+          "react-app",
+           {
+            runtime: "automatic",
+           }
+          ]
+        ],
+      }
+     ],
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+      },
+    ],
   },
   transformIgnorePatterns: ["node_modules/(?!react-markdown)/"],
-  modulePaths: [],
-  moduleNameMapper: {
-    "^react-native$": "react-native-web",
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-  },
-  moduleFileExtensions: [
-    "web.js",
-    "js",
-    "web.ts",
-    "ts",
-    "web.tsx",
-    "tsx",
-    "json",
-    "web.jsx",
-    "jsx",
-    "node",
-  ],
-  watchPlugins: [
-    "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname",
-  ],
   resetMocks: true,
 }
