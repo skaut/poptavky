46ba4ff2b15473436fd692e200b0e2952b025296
Marek Dědič <developer@dedic.eu>
2025-01-01 16:14:46 +0100

    [collector] Disabled naming-convention eslint rule

--- packages/collector/src/octokit.ts
+++ packages/collector/src/octokit.ts
@@ -1,6 +1,5 @@
 import { Octokit } from "@octokit/core";
 import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
 
-// eslint-disable-next-line @typescript-eslint/naming-convention -- Classes should be PascalCase
 const OctokitWithRest = Octokit.plugin(restEndpointMethods);
 export const octokit = new OctokitWithRest({ auth: process.env["PAT"] });
